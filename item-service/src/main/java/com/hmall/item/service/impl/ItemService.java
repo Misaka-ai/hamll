4b9fbf8e36dccf06e0988d426bf892aea9ad3cf3
Misaka-ai <2448123830@qq.com>
2024-03-18 21:54:17 +0800

    初步完成分页功能

--- item-service/src/main/java/com/hmall/item/service/impl/ItemService.java
+++ item-service/src/main/java/com/hmall/item/service/impl/ItemService.java
@@ -1,11 +1,29 @@
 package com.hmall.item.service.impl;
 
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmall.common.dto.PageDTO;
 import com.hmall.item.mapper.ItemMapper;
 import com.hmall.item.pojo.Item;
 import com.hmall.item.service.IItemService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
+@Slf4j
 public class ItemService extends ServiceImpl<ItemMapper, Item> implements IItemService {
+    @Autowired
+    private ItemMapper itemMapper;
+
+    @Override
+    public PageDTO<Item> pageQuery(Integer page, Integer size) {
+        Page<Item> itemPage = new Page<>(page, size);
+        itemMapper.selectPage(itemPage, null);
+        PageDTO<Item> itemPageDTO = new PageDTO<>();
+        itemPageDTO.setTotal(itemPage.getTotal());
+        itemPageDTO.setList(itemPage.getRecords());
+
+        return itemPageDTO;
+    }
 }
--- item-service/src/main/java/com/hmall/item/web/ItemController.java
+++ item-service/src/main/java/com/hmall/item/web/ItemController.java
@@ -1,7 +1,10 @@
 package com.hmall.item.web;
 
+import com.hmall.common.dto.PageDTO;
+import com.hmall.item.pojo.Item;
 import com.hmall.item.service.IItemService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,4 +14,12 @@ public class ItemController {
 
     @Autowired
     private IItemService itemService;
+
+    /*
+     * 分页查询
+     * */
+    @GetMapping("list")
+    PageDTO<Item> list(Integer page, Integer size) {
+        return itemService.pageQuery(page,size);
+    }
 }
