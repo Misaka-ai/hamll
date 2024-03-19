557c5d545f7f64f4de1734b1238241d3b5dc3dc2
Misaka-ai <2448123830@qq.com>
2024-03-19 20:57:54 +0800

    创建了search-service和导入了ItemDOc

---
+++ item-service/src/main/java/com/hmall/item/pojo/ItemDoc.java
@@ -0,0 +1,44 @@
+package com.hmall.item.pojo;
+
+
+
+import lombok.Data;
+
+
+import java.util.Arrays;
+import java.util.List;
+
+@Data
+public class ItemDoc {
+
+    private String name;//商品名称
+    private String brand;//品牌
+
+    private String category;//分类
+
+    private Long price;//价格
+
+    private Integer sold;//销售数量
+
+    private Long id;//商品ID
+
+    private String image;//图片
+
+    private Integer commentCount;//评价数
+
+    private List<String> suggestion;
+
+    public  ItemDoc(Item item) {
+        this.id = item.getId();
+        this.name = item.getName();
+        this.price = item.getPrice();
+        this.image = item.getImage();
+        this.category = item.getCategory();
+        this.brand = item.getBrand();
+        this.sold = item.getSold();
+        this.commentCount = item.getCommentCount();
+        this.suggestion = Arrays.asList(this.name, this.brand);
+
+
+    }
+}
